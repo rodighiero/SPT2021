36a6b00f855141f0bbec0dd23b740b32e683b33a
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2021-05-12 14:43:34 +0200

    Library update

--- src/draw/clusters.js
+++ src/draw/clusters.js
@@ -1,41 +1,40 @@
 import { Graphics } from 'pixi.js'
-import { group, polygonHull } from 'd3'
+import { polygonHull } from 'd3'
 
 
 export default (data, clusters) => {
 
-    
+
     const stage = new Graphics()
     stage.interactiveChildren = false
     stage.name = 'clusters'
     s.viewport.addChild(stage)
-    
+
     const groups = clusters.reduce((object, element, index, a) => {
+        const coordinates = [data[index][0], data[index][1]]
         if (object[element])
-            object[element].push(data[index])
+            object[element].push(coordinates)
         else
-        object[element] = [[data[index][0], data[index][1]]]
+            object[element] = [coordinates]
         return object
     }, {})
-
-    // console.log(groups)
 
     const width = 1
     const color = 0xFFFFFF
 
     Object.values(groups).forEach(group => {
 
-        // const points = cluster.map(node => [node.x, node.y])
         const polygon = polygonHull(group)
 
         stage.lineStyle(width, color)
-
         stage.beginFill(color, 0.1)
 
         if (polygon) {
             polygon.forEach((point, i) => {
-                if (i == 0) stage.moveTo(point[0], point[1])
-                stage.lineTo(point[0], point[1])
+                if (i == 0)
+                    stage.moveTo(point[0], point[1])
+                else
+                    stage.lineTo(point[0], point[1])
             })
             stage.closePath()
         }
