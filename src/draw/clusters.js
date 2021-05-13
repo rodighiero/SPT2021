01b014085b9074ed08f5ff2f1d0a28f41c6efa52
Dario Rodighiero <2089004+rodighiero@users.noreply.github.com>
2021-05-13 13:01:53 +0200

    Cluster workflow simplified

--- src/draw/clusters.js
+++ src/draw/clusters.js
@@ -10,21 +10,14 @@ export default (data, clusters) => {
     stage.name = 'clusters'
     s.viewport.addChild(stage)
 
-    const groups = clusters.reduce((object, element, index, a) => {
-        const coordinates = [data[index][0], data[index][1]]
-        if (object[element])
-            object[element].push(coordinates)
-        else
-            object[element] = [coordinates]
-        return object
-    }, {})
-
     const width = 1
     const color = 0xFFFFFF
 
-    Object.values(groups).forEach(group => {
+    Object.values(clusters).forEach(cluster => {
 
-        const polygon = polygonHull(group)
+        cluster = cluster.map(index => [data[index][0], data[index][1]])
+
+        const polygon = polygonHull(cluster)
 
         stage.lineStyle(width, color)
         stage.beginFill(color, 0.1)
